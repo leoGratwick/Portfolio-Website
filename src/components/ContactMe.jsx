5cbf23f05a303c54bf97d3991beae7d3600a174a
Leo Gratwick <xm22456@bristol.ac.uk>
2023-10-21 20:34:27 +0100

    added email js

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,21 +1,71 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactMe = () => {
+  const form = useRef();
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    emailAdress: "",
+    phoneNumber: "",
+    message: "",
+  });
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_t718xaq",
+        "template_0dutpzc",
+        form.current,
+        "oEOrlsPcC_wE3uo5s"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setFormData({
+            firstName: "",
+            lastName: "",
+            emailAdress: "",
+            phoneNumber: "",
+            message: "",
+          });
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   return (
     <section id="Contact" className="contact--section">
       <div>
         <h2 className="section-title contact-title">Contact Me</h2>
       </div>
-      <form className="contact--form--container">
-        
+      <form
+        ref={form}
+        onSubmit={sendEmail}
+        className="contact--form--container"
+      >
         <div className="details-container">
           <label htmlFor="first-name" className="contact-label">
             <span className="text-md">First Name</span>
             <input
               type="text"
               className="contact-input "
-              name="first-name"
+              name="firstName"
+              value={formData.firstName}
               id="first-name"
+              onChange={handleChange}
               required
             />
           </label>
@@ -24,8 +74,10 @@ const ContactMe = () => {
             <input
               type="text"
               className="contact-input "
-              name="last-name"
+              name="lastName"
+              value={formData.lastName}
               id="last-name"
+              onChange={handleChange}
               required
             />
           </label>
@@ -34,8 +86,10 @@ const ContactMe = () => {
             <input
               type="email"
               className="contact-input "
-              name="email"
+              name="emailAdress"
+              value={formData.emailAdress}
               id="email"
+              onChange={handleChange}
               required
             />
           </label>
@@ -44,34 +98,37 @@ const ContactMe = () => {
             <input
               type="number"
               className="contact-input "
-              name="phone-number"
-              id="phone-number"
-              required
+              name="phoneNumber"
+              value={formData.phoneNumber}
+              id="phoneNumber"
+              onChange={handleChange}
             />
           </label>
         </div>
-        <div className='message-and-button'>
-        <label htmlFor="message" className="message contact-label">
-          <span className="text-md">Message</span>
-          <textarea
-            className="contact-input text-md"
-            id="message"
-            rows="8"
-            placeholder="Type your message ..."
-          />
-        </label>
-        
-        <div>
+        <div className="message-and-button">
+          <label htmlFor="message" className="message contact-label">
+            <span className="text-md">Message</span>
+            <textarea
+              className="contact-input text-md"
+              id="message"
+              rows="8"
+              name="message"
+              value={formData.message}
+              placeholder="Type your message ..."
+              onChange={handleChange}
+            />
+          </label>
+
+          <div>
             <button className="btn ">Submit</button>
-        </div>
+          </div>
         </div>
         <div className="contact-form-img-container">
-          <img className="contact-form-img"  src= "./img/spaceCat.png"/>
+          <img className="contact-form-img" src="./img/spaceCat.png" />
         </div>
-        
       </form>
     </section>
-  )
-}
+  );
+};
 
-export default ContactMe
+export default ContactMe;
