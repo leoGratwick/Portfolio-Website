02394d39293b08561ec8db9b1d808f6034454503
leoGratwick <lgratwick1@outlook.com>
2024-09-14 14:41:43 +0100

    2nd to 3rd year

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -10,7 +10,7 @@ const AboutMe = () => {
         <div className="about-me-description">
           <h3>Computer Science Student</h3>
           <p>
-            Hi!  Thanks for coming to my website! I'm Leo, a 2nd year Computer Science with Innovation student at the University of Bristol. I love solving complex problems, learning new skills and meeting new people. In my free time I like doing programming projects and art. I acctually drew everthing you see on this website!
+            Hi!  Thanks for coming to my website! I'm Leo, a 3rd year Computer Science with Innovation student at the University of Bristol. I love solving complex problems, learning new skills and meeting new people. In my free time I like doing programming projects and art. I acctually drew everthing you see on this website!
           </p>
         </div>
       </div>
